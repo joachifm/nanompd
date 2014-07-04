80cd5d69725bdcd8f925b77ab84c8955435b4123
Joachim Fasting <joachifm@fastmail.fm>
2014-07-04 14:51:48 +0200

    Add a simple torture benchmark

---
+++ bench/torture.hs
@@ -0,0 +1,15 @@
+{-
+A simple torture benchmark: run some command a ridiculous
+number of times against a running MPD instance.
+-}
+
+module Main (main) where
+
+import MPD
+
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (replicateM_)
+
+main :: IO ()
+main = replicateM_ 1000 $
+  run ((,,) <$> listallinfo <*> status <*> currentsong) >>= ({-# SCC "main/print" #-} print)
