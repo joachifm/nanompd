7349e7c1266f039e6010e2090e53c2c3275d942d
Joachim Fasting <joachifm@fastmail.fm>
2014-07-05 09:42:07 +0200

    MPD: provide a data type for song objects and an associated fold

--- src/MPD.hs
+++ src/MPD.hs
@@ -13,18 +13,59 @@ module MPD
   ) where
 
 import MPD.Core
+import MPD.Util
+
+import Control.DeepSeq (NFData(..), deepseq)
+
+import qualified Data.List           as L
+import qualified Data.HashMap.Strict as M
+
 import Data.ByteString (ByteString)
+import Data.Text (Text)
 
 ------------------------------------------------------------------------
+-- MPD protocol command API.
 
 ping :: Command ()
 ping = Command ["ping"] (return ())
 
-currentsong :: Command [ByteString]
-currentsong = Command ["currentsong"] id_
+currentsong :: Command SongInfo
+currentsong = Command ["currentsong"] (liftFold songInfo)
 
 status :: Command [ByteString]
 status = Command ["status"] id_
 
 listallinfo :: Command [ByteString]
 listallinfo = Command ["listallinfo"] id_
+
+------------------------------------------------------------------------
+-- MPD protocol objects.
+
+data SongInfo = SongInfo
+  { songFile :: {-# UNPACK #-} !Text
+  , songId   :: {-# UNPACK #-} !Text
+  , songPos  :: {-# UNPACK #-} !Text
+  , songTags :: !(M.HashMap ByteString Text)
+  } deriving (Show)
+
+instance NFData SongInfo where
+  rnf x = songFile x `deepseq`
+          songId x   `deepseq`
+          songPos x  `deepseq`
+          songTags x `deepseq` ()
+
+songInfo :: [ByteString] -> SongInfo
+songInfo = L.foldl' step initial
+  where
+    step z x = case pair x of
+      ("file", v) -> z { songFile = v }
+      ("Id", v)   -> z { songId = v }
+      ("Pos", v)  -> z { songPos = v }
+      (k, v)      -> z { songTags = M.insert k v (songTags z) }
+
+    initial = SongInfo
+      { songFile = ""
+      , songId = ""
+      , songPos = ""
+      , songTags = M.empty
+      }
