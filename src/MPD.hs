3b20588f30033bed3fce78a58b197241abe492ed
Joachim Fasting <joachifm@fastmail.fm>
2014-07-14 18:16:18 +0200

    Add haddocks to the top-level module

--- src/MPD.hs
+++ src/MPD.hs
@@ -1,11 +1,51 @@
+{-|
+Module      : MPD
+Copyright   : (c) Joachim Fasting
+License     : MIT
+
+Maintainer  : joachifm@fastmail.fm
+Stability   : experimental
+Portability : portable
+
+This module defines the top-level client API,
+intended for consumption by end users and client
+implementors.
+-}
+
 module MPD
   (
+    -- * Usage
+    -- $usage
+
+    -- * Running MPD protocol command wrappers
     Command
   , run
   , runWith
 
+    -- * Pre-defined MPD protocol command wrappers
   , module MPD.Commands
   ) where
 
 import MPD.Commands
 import MPD.Core
+
+{-$usage
+
+The module should be imported qualified to avoid
+name clashes with the standard "Prelude".
+
+A basic usage example: add a database URI to the current
+playlist, begin playback, and print a crude report comprising
+current song information and daemon status information:
+
+@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified MPD
+import Control.Applicative
+
+foo = do
+  (cur, sta) \<- MPD.run (MPD.add ["music/foo"] *\> MPD.play Nothing *\> ((,) \<$\> MPD.currentSong \<*\> MPD.status))
+  putStr . unlines $ map show [ cur, sta ]
+@
+-}
