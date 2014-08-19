83fffe76384489aa957bf4b2b4e7e66ca469813c
Joachim Fasting <joachifm@fastmail.fm>
2014-08-19 18:29:19 +0200

    Add CommandStrSpec

---
+++ test/MPD/CommandStrSpec.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module MPD.CommandStrSpec (spec) where
+
+import MPD
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "CommandStr" $ do
+    it "singleton" $ do
+      render "foo_cmd" == "foo_cmd"
+
+    it "scalar" $ do
+      render ("foo_cmd" .+ (10 :: Int)) == "foo_cmd 10"
+
+    it "optional/Just" $ do
+      render ("foo_cmd" .+ (Just 10 :: Maybe Int)) == "foo_cmd 10"
+
+    it "optional/Nothing" $ do
+      render ("foo_cmd" .+ (Nothing :: Maybe Int)) == "foo_cmd"
+
+    it "choice/Left" $ do
+      render ("foo_cmd" .+ (Left "foo" :: Either Text Int)) == "foo_cmd foo"
+
+    it "choice/Right" $ do
+      render ("foo_cmd" .+ (Right 10 :: Either Text Int)) == "foo_cmd 10"
