bd33c964acb50fab26d409d0fde1c4e492440f7c
Joachim Fasting <joachifm@fastmail.fm>
2014-08-19 18:50:27 +0200

    More work on ParseBench

--- bench/ParseBench.hs
+++ bench/ParseBench.hs
@@ -4,18 +4,23 @@ import MPD
 import Criterion.Main
 
 main = defaultMain [
-    bench "boolP/valid" (whnf boolP "0")
-  , bench "boolP/invalid" (whnf boolP "foo")
+    bench "boolP/valid" $ whnf boolP "0"
+  , bench "boolP/invalid" $ whnf boolP "foo"
 
-  , bench "intP/valid" (whnf intP "1337")
-  , bench "intP/invalid" (whnf intP "foo")
+  , bench "intP/valid" $ whnf intP "1337"
+  , bench "intP/invalid" $ whnf intP "foo"
 
-  , bench "doubleP/valid" (whnf doubleP "1337.5")
-  , bench "doubleP/invalid" (whnf doubleP "foo")
+  , bench "doubleP/valid" $ whnf doubleP "1337.5"
+  , bench "doubleP/invalid" $ whnf doubleP "foo"
 
-  , bench "textP" (whnf textP "foo")
+  , bench "textP" $ whnf textP "foo"
 
-  , bench "labelP" (whnf labelP "foo")
-  , bench "pathP" (whnf pathP "foo/bar/baz.mp3")
-  , bench "dateP" (whnf dateP "2014-05-16T17:33:26Z")
+  , bench "labelP" $ whnf labelP "foo"
+  , bench "pathP" $ whnf pathP "foo/bar/baz.mp3"
+  , bench "dateP" $ whnf dateP "2014-05-16T17:33:26Z"
+
+  , bench "field/single" $
+      whnf (parse (field "key" intP)) ["key: 1337"]
+  , bench "field/compound" $
+      whnf (parse ((,) <$> field "a" intP <*> field "b" boolP)) ["a: 1337", "b: 0"]
   ]
