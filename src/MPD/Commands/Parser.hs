b02377ed5feaec235b8b8cc2855a554a476193d5
Joachim Fasting <joachifm@fastmail.fm>
2014-08-23 13:10:18 +0200

    MPD.Commands.Parser: add missing tag types

--- src/MPD/Commands/Parser.hs
+++ src/MPD/Commands/Parser.hs
@@ -106,4 +106,22 @@ songTag :: Parser (Label, Text)
 songTag = foldr1 (<|>) $ map (`pair` textP) tagTypes
 
 tagTypes :: [Label]
-tagTypes = [ "Artist", "Title", "Album" ]
+tagTypes = [
+    "Artist"
+  , "ArtistSort"
+  , "Album"
+  , "AlbumArtist"
+  , "AlbumArtistSort"
+  , "Title"
+  , "Track"
+  , "Name"
+  , "Genre"
+  , "Date"
+  , "Composer"
+  , "Performer"
+  , "Disc"
+  , "MUSICBRAINZ_ARTISTID"
+  , "MUSICBRAINZ_ALBUMID"
+  , "MUSICBRAINZ_ALBUMARTISTID"
+  , "MUSICBRAINZ_TRACKID"
+  ]
