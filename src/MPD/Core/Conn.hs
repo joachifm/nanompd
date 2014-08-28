f580e2b6b132b256c7082609bab5f037077e085d
Joachim Fasting <joachifm@fastmail.fm>
2014-08-28 19:49:40 +0200

    More precise exception handling
    Instead of lifting all exceptions that occur during connection IO,
    lift only those exceptions that indicate a problem with the
    connection and rethrow all others.

--- src/MPD/Core/Conn.hs
+++ src/MPD/Core/Conn.hs
@@ -32,6 +32,7 @@ import Control.Monad (unless, void)
 import Control.Monad.Trans (MonadIO(..))
 import Control.Monad.Trans.Either (EitherT(..), left, right)
 import qualified Control.Monad.Catch as C
+import qualified GHC.IO.Exception as GE
 
 import System.IO (Handle, hClose)
 import System.IO.Error (tryIOError)
@@ -47,7 +48,7 @@ import Network (HostName, PortID(..), connectTo)
 -- Sending requests and receiving responses
 
 getResponse
-  :: (MonadIO m)
+  :: (C.MonadThrow m, MonadIO m)
   => Handle
   -> [CommandStr]
   -> Parser a
@@ -56,7 +57,11 @@ getResponse hdl q p = do
   send hdl q
   either (left . ParseError) right =<< parse p <$> recv hdl
 
-send :: (MonadIO m) => Handle -> [CommandStr] -> EitherT ClientError m ()
+send
+  :: (C.MonadThrow m, MonadIO m)
+  => Handle
+  -> [CommandStr]
+  -> EitherT ClientError m ()
 send hdl = connIO . SB.hPut hdl . pack
 
 pack :: [CommandStr] -> SB.ByteString
@@ -65,7 +70,10 @@ pack = T.encodeUtf8 . T.unlines . f . map render
     f [x] = [x]
     f xs  = ("command_list_ok_begin" : xs) ++ ["command_list_end"]
 
-recv :: (MonadIO m) => Handle -> EitherT ClientError m [SB.ByteString]
+recv
+  :: (C.MonadThrow m, MonadIO m)
+  => Handle
+  -> EitherT ClientError m [SB.ByteString]
 recv hdl = go
   where
     go = do
@@ -89,7 +97,7 @@ withConn h p = EitherT . C.bracket (runEitherT $ open h p) (e close) . e
     e f = either (return . Left) (runEitherT . f . fst)
 
 open
-  :: (MonadIO m)
+  :: (C.MonadThrow m, MonadIO m)
   => HostName
   -> PortID
   -> EitherT ClientError m (Handle, SB.ByteString)
@@ -101,15 +109,21 @@ open host port = do
     left InvalidHost
   return (hdl, ver)
 
-close :: (MonadIO m) => Handle -> EitherT ClientError m ()
+close :: (C.MonadThrow m, MonadIO m) => Handle -> EitherT ClientError m ()
 close hdl = void . liftIO $
   tryIOError (SB.hPut hdl "close\n") >> tryIOError (hClose hdl)
 
 ------------------------------------------------------------------------
 -- Internal helpers
 
-connIO :: (MonadIO m) => IO a -> EitherT ClientError m a
-connIO m = either (left . ConnError) return =<< liftIO (tryIOError m)
+connIO :: (C.MonadThrow m, MonadIO m) => IO a -> EitherT ClientError m a
+connIO m = either handler return =<< liftIO (tryIOError m)
+  where
+    handler eh | isConnError eh = left (ConnError eh)
+               | otherwise      = C.throwM eh
+
+    isConnError eh = GE.ioe_type eh
+      `elem` [ GE.ResourceVanished, GE.EOF ]
 
 readProtocolError :: SB.ByteString -> ClientError
 readProtocolError = either ParseError id . A.parseOnly protocolError
