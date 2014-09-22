5c90d8c68fa20368f4ed0018b73cfdfda4a08d38
Joachim Fasting <joachifm@fastmail.fm>
2014-09-22 14:40:24 +0200

    Re-define default.nix using myEnvFun

--- default.nix
+++ default.nix
@@ -1,28 +1,20 @@
-# A Nix expression that defines a working environment for our library.
+# Usage
 #
-# Run `nix-build` to build the library and `nix-shell` to enter
-# the environment (with `cabal-install` provided for convenience).
-#
-# The expression is parameterised over the set of Haskell packages
-# to build against.
-# By default, build against the latest GHC with profiling enabled.
-#
-# The body comprises a call to `cabal.mkDerivation`, which is passed a
-# recursive (in this case) set of attributes that affect the cabal
-# build process.
-# The set is made recursive so that we can refer to `buildDepends` in
-# the definition of `testDepends`.
+#     $ nix-build
+#     $ ./result/bin/load-env-nanompd-dev
 
-{ haskellPackages ? (import <nixpkgs>{}).haskellPackages_ghc783_profiling }:
-with haskellPackages; cabal.mkDerivation (self: rec {
-  pname = "mpd";
-  version = "0.0.0.0";
-  src = ./.;
-  propagatedBuildDepends = [ cabalInstall ];
-  buildDepends = [
-    cabalInstall attoparsec either exceptions mtl network text
-    unorderedContainers
+let
+  pkgs = import <nixpkgs>{};
+  haskell = pkgs.haskellPackages_ghc783;
+in
+pkgs.myEnvFun {
+  name = "nanompd-dev";
+  buildInputs = [
+    (haskell.ghcWithPackages(self: with self; [
+      cabalInstall attoparsec either exceptions mtl
+      network text unorderedContainers
+      hspec hspecExpectations QuickCheck
+      criterion
+    ]))
   ];
-  testDepends = buildDepends ++ [ hspec hspecExpectations QuickCheck ];
-  doCheck = true;
-})
+}
