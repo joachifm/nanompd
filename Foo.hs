ec690dcb5ea82a3b3fd4582b84d13773ed15ce5f
Joachim Fasting <joachifm@fastmail.fm>
2015-02-26 19:13:40 +0100

    Work on Foo

--- Foo.hs
+++ Foo.hs
@@ -1,6 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-binds #-}
 
-module Foo where
+module Foo () where
+
+import Data.Void
+
+import Control.Monad.Cont (ContT(..))
+import Control.Monad.Identity
+import Control.Monad.Trans
+import Control.Monad.Trans.Except (ExceptT(..))
+import Control.Monad.Trans.Free
+import qualified Control.Monad.Cont as Cont
+import qualified Control.Monad.Trans.Except as Except
+
+import Pipes
+import qualified Pipes.Attoparsec as PA
+import qualified Pipes.Parse  as PP
+import qualified Pipes.Group as PG
+import qualified Pipes.ByteString as PB
 
 import Control.Applicative
 import Control.Monad ((>=>), (<=<), join)
@@ -8,9 +25,9 @@ import Control.Monad ((>=>), (<=<), join)
 import Data.Function
 import Data.Semigroup
 
-import Data.Attoparsec.ByteString.Char8
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as SB
+import qualified Data.Attoparsec.ByteString.Char8 as A
 
 import System.IO
 import System.IO.Error
@@ -20,21 +37,21 @@ import Network
 ------------------------------------------------------------------------
 -- Parser
 
-pair :: ByteString -> Parser a -> Parser (ByteString, a)
-pair k v = (,) <$> string k <* string ": " <*> v <* char '\n'
+pair :: ByteString -> A.Parser a -> A.Parser (ByteString, a)
+pair k v = (,) <$> A.string k <* A.string ": " <*> v <* A.char '\n'
 
-field :: ByteString -> Parser a -> Parser a
+field :: ByteString -> A.Parser a -> A.Parser a
 field k v = snd <$> pair k v
 
-response :: Parser a -> Parser (Either ByteString a)
+response :: A.Parser a -> A.Parser (Either ByteString a)
 response p =
   Right <$> (p <* "list_OK\n") <|>
-  Left  <$> ("ACK " *> takeWhile1 (/= '\n') <* char '\n')
+  Left  <$> ("ACK " *> A.takeWhile1 (/= '\n') <* A.char '\n')
 
-object :: Parser (ByteString, Int)
+object :: A.Parser (ByteString, Int)
 object =
-  (,) <$> field "file" (takeWhile1 (/= '\n'))
-      <*> field "time" decimal
+  (,) <$> field "file" (A.takeWhile1 (/= '\n'))
+      <*> field "time" A.decimal
 
 ------------------------------------------------------------------------
 -- Dummy input
@@ -44,16 +61,16 @@ dummy1 = "file: foo.mp3\ntime: 420\nlist_OK\n"
 dummy2 = "file: bar.mp3\ntime: 120\nlist_OK\n"
 dummy3 = "ACK protocol error\n"
 
-test1 = parse (response object) dummy1
-test2 = parse (response object) dummy3
-test3 = parse ((,) <$> response object <*> response object) (dummy1 <> dummy2)
-test4 = parse ((,) <$> response object <*> response object) dummy1
-test5 = parse ((,) <$> response object <*> response object) (dummy1 <> dummy3)
-test6 = parse (response object) (dummy1 <> dummy2)
-test7 = parse (response object) (dummy1 <> dummy3)
+test1 = A.parse (response object) dummy1
+test2 = A.parse (response object) dummy3
+test3 = A.parse ((,) <$> response object <*> response object) (dummy1 <> dummy2)
+test4 = A.parse ((,) <$> response object <*> response object) dummy1
+test5 = A.parse ((,) <$> response object <*> response object) (dummy1 <> dummy3)
+test6 = A.parse (response object) (dummy1 <> dummy2)
+test7 = A.parse (response object) (dummy1 <> dummy3)
 
 ------------------------------------------------------------------------
--- Commands
+-- Core
 
 connect = do
   h <- connectTo "localhost" (PortNumber 6600)
@@ -67,28 +84,33 @@ pack = SB.unlines
      . (++ ["command_list_end"])
      . filter (not . SB.null)
 
-run q p k = do
+close h = SB.hPut h "close\n" >> hClose h
+
+------------------------------------------------------------------------
+-- Commands
+
+command :: ByteString -> A.Parser a -> ([ByteString], A.Parser a)
+command q p = ([q], p <* "list_OK\n")
+
+simple (q, p) k = do
   (h, _) <- connect
-  r <- runWith h q p k
+  r <- runWith h (q, p <* "OK\n") k
   hClose h
   return r
 
-runWith h q p k = do
+runWith h (q, p) k = do
   SB.hPut h (pack q)
-  process h (p <* "OK\n") k
+  process h p k
 
-recv h = SB.hGetSome h 64
+process h p k = start recv p k
+  where recv = SB.hGetSome h 64
 
-process h p k = start (recv h) p k
-
-start g p k =
-  worker g k (parse p "")
+start g p k = worker g k (A.parse p "")
 
 worker g k = fix $ \recur p -> do
-  print p
   ln <- g
-  case feed p ln of
-    Partial p' -> recur (Partial p')
-    Done "" r  -> k r
-    Done i _   -> fail ("superflous input: " ++ show i)
-    Fail i _ s -> fail ("failed parsing " ++ show i ++ "; " ++ s)
+  case A.feed p ln of
+    A.Partial p' -> recur (A.Partial p')
+    A.Done "" r  -> k r
+    A.Done i _   -> fail ("superflous input: " ++ show i)
+    A.Fail i _ s -> fail ("failed parsing " ++ show i ++ "; " ++ s)
